06e8c9f6032edcc74a510d959c7ec92122625590
Sergei Fomin <sergeifomin83@inbox.ru>
2020-12-20 19:09:42 +0300

    added usage of user dialog

--- src/components/PromptPage/index.js
+++ src/components/PromptPage/index.js
@@ -1,13 +1,22 @@
 import { memo } from "react";
 import useCustomPrompt from '../../hooks/useCustomPrompt';
+import {Dialog} from 'evergreen-ui';
 
 const PromptPage = () => {
-  const {CustomPrompt} = useCustomPrompt();
+  const {CustomPrompt, modalIsActive, onConfirm, onCancel} = useCustomPrompt();
 
   return (
     <div>
       <h1>The Page with Prompt. Try to leave it!</h1>
       <CustomPrompt 
+        modal={Dialog}
+        modalProps={{
+          hasHeader: false,
+          isShown: modalIsActive,
+          onConfirm,
+          onCancel,
+          children: 'Do you want to leave the page?',
+        }}
         message={'Вы действительно хотите покинуть страницу'}
         yesButton={'Да'}
         noButton={'Нет'}/>
